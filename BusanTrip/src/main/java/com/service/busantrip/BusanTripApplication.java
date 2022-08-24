84696470b0983b25422a95869c6038f7a75cb801
ssy0061 <ssafy0061@gmail.com>
2022-08-24 12:18:20 +0900

    setting SrpingBoot

---
+++ BusanTrip/src/main/java/com/service/busantrip/BusanTripApplication.java
@@ -0,0 +1,13 @@
+package com.service.busantrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusanTripApplication.class, args);
+	}
+
+}
---
+++ BusanTrip/src/test/java/com/service/busantrip/BusanTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.busantrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusanTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
