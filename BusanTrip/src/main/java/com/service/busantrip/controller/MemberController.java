4e2ca4b4dbf599af0425b1aa9fd9b6760551a5e6
choseoyeong <se5253@naver.com>
2022-08-29 22:32:26 +0900

    add_url

--- BusanTrip/src/main/java/com/service/busantrip/controller/MemberController.java
+++ BusanTrip/src/main/java/com/service/busantrip/controller/MemberController.java
@@ -1,8 +1,22 @@
 package com.service.busantrip.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
-
+//	@GetMapping("/")
+//	public String index() {
+//		
+//		return "home/home";
+//	}
+	
+	@GetMapping("/")
+	public String search() {
+		
+		return "search/search";
+	}
+	
+	
 }
